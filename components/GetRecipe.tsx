3f21d72b426a8ad04353af832507d0d1a49b4a1c
wingmatt <matt@wingwebhelp.com>
2021-03-11 07:23:31 -0800

    Set default value for dev. Log response from POST

--- components/GetRecipe.tsx
+++ components/GetRecipe.tsx
@@ -12,12 +12,15 @@ const GetRecipe = () => {
         url: event.target.url.value
       })
     }
-    const recipeData = await fetch('/api/get-recipe/', fetchOptions)
+    const recipeData = await fetch('/api/get-recipe/', fetchOptions).then(response => {
+      console.log(response)
+    })
   };
+  
   return (
   <form onSubmit={getRecipe}>
     <label htmlFor="url">Recipe URL</label>
-    <input type="text" id="url" name="url" />
+    <input type="text" id="url" name="url" defaultValue="https://wingcuisine.com/recipes/recipe/smoked-gouda-fusilli-ham/"/>
   </form>
   )
   
