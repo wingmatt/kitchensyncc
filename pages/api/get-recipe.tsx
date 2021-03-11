a12ad238336a814ceb8717788ff2197cc54f3c4b
wingmatt <matt@wingwebhelp.com>
2021-03-11 07:28:57 -0800

    Prettier formatting

--- pages/api/get-recipe.tsx
+++ pages/api/get-recipe.tsx
@@ -1,28 +1,32 @@
-import Cors from 'cors'
+import Cors from "cors";
 
 const cors = Cors({
-  methods: ['GET', 'HEAD'],
-})
+  methods: ["GET", "HEAD"],
+});
 
 function runMiddleware(req, res, fn) {
   return new Promise((resolve, reject) => {
     fn(req, res, (result) => {
       if (result instanceof Error) {
-        return reject(result)
+        return reject(result);
       }
 
-      return resolve(result)
-    })
-  })
+      return resolve(result);
+    });
+  });
 }
 
 async function handler(req, res) {
   // Run the middleware
-  await runMiddleware(req, res, cors)
 
   // Rest of the API logic
   const recipeUrl = req.body.url;
-  return await fetch(recipeUrl).then(response => response.text().then(responseHtml => responseHtml))
+  return await fetch(recipeUrl).then((response) =>
+    response.text().then((responseHtml) => {
+      console.log(responseHtml);
+      return responseHtml;
+    })
+  );
 }
 
-export default handler
+export default handler;
