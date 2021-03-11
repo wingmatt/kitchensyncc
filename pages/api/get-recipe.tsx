4d74ec918ea87c12666d567144493a6f4b2b0a18
wingmatt <matt@wingwebhelp.com>
2021-03-11 07:22:48 -0800

    Setup API Route to fetch HTML from a given URL

--- pages/api/get-recipe.tsx
+++ pages/api/get-recipe.tsx
@@ -1,7 +1,28 @@
 import Cors from 'cors'
 
-export const config = {
-  api: {
-    
-  },
+const cors = Cors({
+  methods: ['GET', 'HEAD'],
+})
+
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
 }
+
+async function handler(req, res) {
+  // Run the middleware
+  await runMiddleware(req, res, cors)
+
+  // Rest of the API logic
+  const recipeUrl = req.body.url;
+  return await fetch(recipeUrl).then(response => response.text().then(responseHtml => responseHtml))
+}
+
+export default handler
