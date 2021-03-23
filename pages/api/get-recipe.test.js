1532a111f1165ebb85c09d8365bc5a56650172eb
wingmatt <matt@wingwebhelp.com>
2021-03-22 17:26:30 -0700

    Add notes on how to make test meaningful

--- pages/api/get-recipe.test.js
+++ pages/api/get-recipe.test.js
@@ -3,6 +3,19 @@ import GetRecipe from '../../components/GetRecipe'
 import '@testing-library/jest-dom/extend-expect'
 
 test('Entering a recipe URL returns a 200 response', async ()=> {
+  // Arrange
   render(<GetRecipe />)
+
+  // Act
   fireEvent.click(screen.getByText('Clip Recipe'))
+
+  // TODO Assert
+    // This conjures and clicks the button, but how can I assert what the response should be?
+    // Right now the RecipeJson just gets sent to the console log. That data needs to get into the component somehow. 
+    // Probably as state that can be scooted upstream.
+
+    // 
+})
+test('Submitting a URL returns an Ingredients array', async () => {
+  render(<GetRecipe />)
 })
