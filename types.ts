5a7ca728b7abff51c0ae3f002a96549d6d583bea
wingmatt <matt@wingwebhelp.com>
2021-03-23 00:01:19 -0700

    Add Ingredient type matching parser response obj

--- types.ts
+++ types.ts
@@ -2,3 +2,11 @@ export interface Recipe {
   url: string,
   ingredients: string[]
 }
+
+export interface Ingredient {
+  quantity: number,
+  unit: string,
+  ingredient: string,
+  minQty: number,
+  maxQty: number
+}
