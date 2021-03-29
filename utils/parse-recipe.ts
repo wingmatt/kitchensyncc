788de6a2e0009d7baba63e4304375287a398616f
wingmatt <matt@wingwebhelp.com>
2021-03-29 08:22:52 -0700

    Outline parseRecipe function

--- utils/parse-recipe.ts
+++ utils/parse-recipe.ts
@@ -23,4 +23,15 @@ function getRecipeDataFromHtml(html: string): Ingredient[] {
   return ingredients;
 }
 
+// Here it is. The big one that ties it all together!
+// Are you ready for a function that takes an HTML string and plops out an array of Ingredients?
+function parseRecipe (html: string): Ingredient[] {
+// To do this magnificent feat, it will need to check for a JSON LD Recipe schema that includes a recipeIngredient array.
+// If it finds one, it will take that array and parse it into our Ingredient format.
+// If it doesn't find one, it will go through the more tedious process of building the array from DOM elements.
+// If it can't build the array, it will return an error.
+}
+
 export { getRecipeDataFromSchemaJson, getRecipeDataFromHtml }
+
+export default parseRecipe
