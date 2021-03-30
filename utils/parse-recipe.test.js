667b389ccfb1726ed3c15c0bf9e60bee36ae7894
wingmatt <matt@wingwebhelp.com>
2021-03-29 22:30:25 -0700

    Build test scaffolds for 2 main test cases

--- utils/parse-recipe.test.js
+++ utils/parse-recipe.test.js
@@ -1,10 +1,24 @@
-
-import  parseRecipe, { getRecipeDataFromSchemaJson } from "./parse-recipe";
+import parseRecipe, { getRecipeDataFromSchemaJson } from "./parse-recipe";
 import "@testing-library/jest-dom/extend-expect";
-import { Ingredient } from '../types'
 
 test("Schema Parsing returns an array of Ingredient Objects", async () => {
   const testJson = require("../allrecipesSchema.json");
   const parsedJson = getRecipeDataFromSchemaJson(testJson);
-  expect(parsedJson).toEqual(expect.arrayContaining([expect.objectContaining({ingredient: 'eggs'})]));
+  expect(parsedJson).toEqual(
+    expect.arrayContaining([expect.objectContaining({ ingredient: "eggs" })])
+  );
+});
+test("parseRecipe returns an array of Ingredient Objects when supplied with HTML + JSON LD", async () => {
+  // setup test data to match test case
+  const testData = "TODO";
+  expect(parseRecipe(testData)).toEqual(
+    expect.arrayContaining([expect.objectContaining({ ingredient: "eggs" })])
+  );
+});
+test("parseRecipe returns an array of Ingredient Objects when supplied with marked-up HTML", async () => {
+  // setup test data to match test case
+  const testData = "TODO";
+  expect(parseRecipe(testData)).toEqual(
+    expect.arrayContaining([expect.objectContaining({ ingredient: "eggs" })])
+  );
 });
