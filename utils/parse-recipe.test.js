1d17364d7c7b8c8ab73c9e259a031e82dbe67ee2
wingmatt <matt@wingwebhelp.com>
2021-03-31 07:21:34 -0700

    Add MSW and setup mock endpoints

--- utils/parse-recipe.test.js
+++ utils/parse-recipe.test.js
@@ -1,5 +1,6 @@
 import parseRecipe, { getRecipeDataFromSchemaJson } from "./parse-recipe";
 import "@testing-library/jest-dom/extend-expect";
+import getRecipeAPI from "../pages/api/get-recipe"
 
 test("Schema Parsing returns an array of Ingredient Objects", async () => {
   const testJson = require("../allrecipesSchema.json");
@@ -10,15 +11,13 @@ test("Schema Parsing returns an array of Ingredient Objects", async () => {
 });
 test("parseRecipe returns an array of Ingredient Objects when supplied with HTML + JSON LD", async () => {
   // setup test data to match test case
-  const testData = "TODO";
-  expect(parseRecipe(testData)).toEqual(
+  expect(parseRecipe('/mocks/json')).toEqual(
     expect.arrayContaining([expect.objectContaining({ ingredient: "eggs" })])
   );
 });
 test("parseRecipe returns an array of Ingredient Objects when supplied with marked-up HTML", async () => {
   // setup test data to match test case
-  const testData = "TODO";
-  expect(parseRecipe(testData)).toEqual(
+  expect(parseRecipe('/mocks/itemprops')).toEqual(
     expect.arrayContaining([expect.objectContaining({ ingredient: "eggs" })])
   );
 });
