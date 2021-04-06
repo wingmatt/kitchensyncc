b90daaebbf4a3c4f4aa2a33a5b582b619782fcc2
wingmatt <matt@wingwebhelp.com>
2021-04-05 21:18:39 -0700

    Parse HTML-based ingredient data in parseRecipe. Clean up initial comments

--- utils/parse-recipe.ts
+++ utils/parse-recipe.ts
@@ -10,12 +10,11 @@ function parseHtml(html: string): any {
   return htmlParser(html);
 }
 
-function getRecipeDataFromSchemaJson(json: any[]): Ingredient[] {
-  const recipeIngredient = json.find(
-    (schemaType) => schemaType["@type"] == "Recipe"
-  ).recipeIngredient;
+function getRecipeDataFromSchemaJson(json: any): Ingredient[] {
+  const recipeIngredient = json.recipeIngredient;
   return recipeIngredient.map((ingredient) => parse(ingredient));
 }
+
 function getRecipeDataFromHtml(html: string): Ingredient[] {
   const recipeHtml = parseHtml(html);
   const ingredients = [];
@@ -40,20 +39,19 @@ function parseRecipe(html: string): Ingredient[] {
   const jsonRecipe = jsonScripts.find(
     (schemaType) => schemaType["@type"] == "Recipe"
   );
-  console.log("Da HTML: " + jsonScripts);
-
-  // TODO: Handle newline characters within stringified JSON properties, e.g. https://www.seriouseats.com/recipes/2021/03/pasta-alla-norcina-creamy-pasta-with-sausage.html
-  // TODO MAYBE: Make sure there is a Recipe Schema Object if we are going to go down the road of JSON parsing
-
-  // If it finds one, it will take that array and parse it into our Ingredient format.
   if (jsonRecipe) {
     // Right now schemaJson will work for allrecipes, and schemaJsonArray will work for some SeriousEats
-    return getRecipeDataFromSchemaJson(jsonScripts);
+    return getRecipeDataFromSchemaJson(jsonRecipe);
   }
-  // If it doesn't find one, it will go through the more tedious process of building the array from DOM elements.
   else {
+    const htmlRecipe = document
+      .querySelectorAll(
+        '*[itemprop="recipeIngredient"], *[itemprop="ingredients"]'
+      )
+      .map((script) => script.rawText)
+      .flat();
+      console.log(htmlRecipe);
     return getRecipeDataFromHtml(html);
-    // TODO: If it can't build the array, it will return an error.
   }
 }
 
