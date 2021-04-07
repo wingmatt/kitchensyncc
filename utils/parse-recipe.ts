a9005ecd9260066e13ab55b30f7a12647ed9885a
wingmatt <matt@wingwebhelp.com>
2021-04-06 19:34:10 -0700

    Add comments for declarative stmt. Rename parse

--- utils/parse-recipe.ts
+++ utils/parse-recipe.ts
@@ -1,20 +1,20 @@
-import { parse as htmlParser } from "node-html-parser";
-import { parse } from "recipe-ingredient-parser-v2";
+import { parse as parseHtml } from "node-html-parser";
+import { parse as parseIngredient } from "recipe-ingredient-parser-v2";
 import { Ingredient } from "../types";
 
 function htmlStringtoDom(html: string): any {
-  return htmlParser(html);
+  return parseHtml(html);
 }
 
 function parseRecipeJson(document: HTMLElement): Ingredient[] | null {
   const jsonRecipe = Array.from(
     document.querySelectorAll('script[type="application/ld+json"]')
-  )
-    .map((script) => JSON.parse(script.innerHTML))
-    .flat()
-    .find((schema) => schema["@type"] == "Recipe");
+  ) // Get all LD JSON script tags
+    .map((script) => JSON.parse(script.innerHTML)) // For each tag, grab the actual JSON within
+    .flat() // Flatten the result — sometimes a script contains an array of objects instead of an object
+    .find((schema) => schema["@type"] == "Recipe"); // Grab the Recipe schema out of all the collected JSON
   if (jsonRecipe) {
-    return jsonRecipe.recipeIngredient.map((ingredient) => parse(ingredient));
+    return jsonRecipe.recipeIngredient.map((ingredient) => parseIngredient(ingredient));
   } else return null;
 }
 
@@ -23,7 +23,7 @@ function parseRecipeHtml(document: HTMLElement): Ingredient[] {
     document.querySelectorAll(
       '*[itemprop="recipeIngredient"], *[itemprop="ingredients"]'
     )
-  ).map((script) => parse(script.textContent));
+  ).map((script) => parseIngredient(script.textContent));
 }
 
 function parseRecipe(html: string): Ingredient[] {
