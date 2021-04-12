aa4dabe332e9136d31d043e29bc4038b6230e8d1
wingmatt <matt@wingwebhelp.com>
2021-04-12 07:46:27 -0700

    Add MenuLink component w/ active link highlight

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,11 +1,11 @@
-import Link from "next/link"
+import MenuLink from "../components/MenuLink"
 
 export default function Menu() {
   return (
     <nav>
-        <Link href="/pantry"><a>Pantry</a></Link>
-        <Link href="/list"><a>Shopping List</a></Link>
-        <Link href="/recipes"><a>Recipes</a></Link>
-      </nav>
+      <MenuLink href="/pantry" label="Pantry" />
+      <MenuLink href="/list" label="Shopping List" />
+      <MenuLink href="/recipes" label="Recipes" />
+    </nav>
   )
 }
