4e51223509df10192b3398cf7a295f2003864cfe
wingmatt <matt@wingwebhelp.com>
2021-04-11 18:02:08 -0700

    Build page routes for list, pantry, recipes urls

---
+++ pages/pantry.tsx
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout'
+
+export default function Pantry(){
+  return (
+    <Layout title="Pantry">
+      <ul>
+        <li>Pantry stuff!</li>
+      </ul>
+    </Layout>
+  )
+}
