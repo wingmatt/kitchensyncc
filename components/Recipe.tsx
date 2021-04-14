eebb9cad03365a26ba8767d8974e70b200bac7eb
wingmatt <matt@wingwebhelp.com>
2021-04-14 08:04:08 -0700

    Add CollapsiblePanel & use within Recipe

--- components/Recipe.tsx
+++ components/Recipe.tsx
@@ -1,21 +1,18 @@
+import { useState } from 'react'
 import { Recipe as RecipeInterface } from "../types";
 import IngredientList from "./IngredientList";
+import CollapsiblePanel from "./CollapsiblePanel"
 
 export default function Recipe(props: RecipeInterface) {
+  const [expanded, setExpanded] = useState(false)
+  
   return (
     <article className="recipe">
       <img src="https://via.placeholder.com/300x200" alt={props.name}></img>
-      <h2>
-        <button aria-expanded="false">{props.name}</button>
-        <svg viewBox="0 0 10 10" focusable="false">
-          <rect className="vert" height="8" width="2" y="1" x="4"/>
-          <rect height="2" width="8" y="4" x="1" />
-        </svg>
-      </h2>
-      <div hidden>
+      <CollapsiblePanel title={props.name}>
         <a href={props.url}>Go to Recipe</a>
         <IngredientList title="Ingredients" ingredients={props.ingredients} />
-      </div>
+      </CollapsiblePanel>
     </article>
   );
 }
