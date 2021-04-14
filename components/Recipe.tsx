44af37999657c303bf411aaf1021627df6ac75d3
wingmatt <matt@wingwebhelp.com>
2021-04-14 08:36:32 -0700

    Style link to look like a button

--- components/Recipe.tsx
+++ components/Recipe.tsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
 import { Recipe as RecipeInterface } from "../types";
 import IngredientList from "./IngredientList";
 import CollapsiblePanel from "./CollapsiblePanel"
 
 export default function Recipe(props: RecipeInterface) {
-  const [expanded, setExpanded] = useState(false)
   
   return (
     <article className="recipe">
       <img src="https://via.placeholder.com/300x200" alt={props.name}></img>
       <CollapsiblePanel title={props.name}>
-        <a href={props.url}>Go to Recipe</a>
+        <a href={props.url} className="button">Go to Recipe</a>
         <IngredientList title="Ingredients" ingredients={props.ingredients} />
       </CollapsiblePanel>
     </article>
