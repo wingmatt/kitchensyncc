bcdc0ab96b133d0f1b2a0d88d87a8c674b99b13f
wingmatt <matt@wingwebhelp.com>
2021-04-15 09:00:10 -0700

    Fill out Ingredient with data & EditableInput

--- components/Ingredient.tsx
+++ components/Ingredient.tsx
@@ -1,11 +1,25 @@
 import { useState } from 'react'
+import {Ingredient as IngredientInterface} from '../types'
+import CollapsiblePanel from './CollapsiblePanel'
 import styles from '../styles/components/Ingredient.module.css'
+import EditableInput from './EditableInput'
 
-export default function Ingredient (props) {
+export default function Ingredient (props: IngredientInterface) {
+  const [editing, setEditing] = useState(false)
   
   return (
   <li className={styles.ingredient}>
-    ⚫ Supplies
+    <CollapsiblePanel title={"⚫ " + props.ingredient}>
+      <ul>
+        <EditableInput editing={editing}>{props.quantity} {props.unit}</EditableInput>
+        <EditableInput editing={editing}>Expires 01/01/1000</EditableInput>
+      </ul>
+      <div className={styles.actions}>
+        <button onClick={() => setEditing(!editing)}>Edit</button>
+        <button>Remove</button>
+      </div>
+    </CollapsiblePanel>
+    
   </li>
   )
 }
