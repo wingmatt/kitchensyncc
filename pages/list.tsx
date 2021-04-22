05544f36e3f59c803b717576a4f8e95a3b9ef31d
wingmatt <matt@wingwebhelp.com>
2021-04-22 08:11:45 -0700

    Add example headings & sections to List page

--- pages/list.tsx
+++ pages/list.tsx
@@ -5,6 +5,21 @@ export default function List(){
   return (
     <Layout title="Shopping List">
       <ul className="content-list">
+        <h2>Produce</h2>
+        <ShoppingListItem quantity="1" unit="cup" ingredient="sugar" type="shopping" />
+        <ShoppingListItem quantity="1" unit="cup" ingredient="sour cream" type="shopping"/>
+        <ShoppingListItem quantity="1" unit="cup" ingredient="Saffron" type="shopping"/>
+        <ShoppingListItem quantity="1" unit="cup" ingredient="Salt" type="shopping"/>
+      </ul>
+      <ul className="content-list">
+        <h2>Spices</h2>
+        <ShoppingListItem quantity="1" unit="cup" ingredient="sugar" type="shopping" />
+        <ShoppingListItem quantity="1" unit="cup" ingredient="sour cream" type="shopping"/>
+        <ShoppingListItem quantity="1" unit="cup" ingredient="Saffron" type="shopping"/>
+        <ShoppingListItem quantity="1" unit="cup" ingredient="Salt" type="shopping"/>
+      </ul>
+      <ul className="content-list">
+        <h2>Dairy</h2>
         <ShoppingListItem quantity="1" unit="cup" ingredient="sugar" type="shopping" />
         <ShoppingListItem quantity="1" unit="cup" ingredient="sour cream" type="shopping"/>
         <ShoppingListItem quantity="1" unit="cup" ingredient="Saffron" type="shopping"/>
