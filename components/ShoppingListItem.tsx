9284284f3b09f762d7379fcdd08b7a04091bf62b
wingmatt <matt@wingwebhelp.com>
2021-04-22 08:07:28 -0700

    Create custom checkbox. Add CSS module

--- components/ShoppingListItem.tsx
+++ components/ShoppingListItem.tsx
@@ -1,22 +1,22 @@
 import { useState } from "react";
 import { Ingredient as IngredientInterface } from "../types";
-import CollapsiblePanel from "./CollapsiblePanel";
-import styles from "../styles/components/Ingredient.module.css";
-import EditableInput from "./EditableInput";
+import styles from "../styles/components/ShoppingListItem.module.css";
+import { FiSquare, FiCheckSquare } from "react-icons/fi"
 
 export default function ShoppingListItem(props: IngredientInterface) {
   const [editing, setEditing] = useState(false);
   const [checked, setChecked] = useState(false);
 
   return (
-    <li className={styles.ingredient}>
+    <li className={styles.listItem}>
       <label>
+        {checked ? <FiCheckSquare/> : <FiSquare/>}
         <input
           type="checkbox"
           checked={checked}
           onChange={() => setChecked(!checked)}
         />
-        {props.ingredient}
+        <span>{props.ingredient}</span>
       </label>
     </li>
   );
