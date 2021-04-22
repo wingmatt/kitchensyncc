8d870d706974484cecf6a21a995f8a09a38570b0
wingmatt <matt@wingwebhelp.com>
2021-04-22 07:36:14 -0700

    Make inputs change depending on ingredient type. Add default status icon for ingredients without a set status.

--- components/EditableInput.tsx
+++ components/EditableInput.tsx
@@ -10,26 +10,12 @@ const Input = function (props) {
         value={input.value}
         onChange={(event) => setInput({ value: event.target.value })}
       ></input>{" "}
-      <InputActions />
     </form>
   );
 };
 
 const Text = function (props) {
   return <span>{props.content}</span>;
-};
-
-const InputActions = function () {
-  // Save button should update the input's saved value on the backend.
-  // Cancel button should revert the input to the saved value.
-  // Both buttons should revert the input back to static text mode.
-  return (
-    <>
-      <button onClick={updateInput} title="Save Edit">
-        ✔
-      </button>
-    </>
-  );
 };
 
 const updateInput = (event) => {
--- pages/pantry.tsx
+++ pages/pantry.tsx
@@ -6,10 +6,10 @@ export default function Pantry(){
   return (
     <Layout title="Pantry">
       <ul className="content-list">
-        <Ingredient quantity="1" unit="cup" ingredient="sugar" status="ok"/>
-        <Ingredient quantity="1" unit="cup" ingredient="Salt" status="warning"/>
-        <Ingredient quantity="1" unit="cup" ingredient="Saffron" status="critical"/>
-
+        <Ingredient quantity="1" unit="cup" ingredient="sugar" status="ok" type="pantry"/>
+        <Ingredient quantity="1" unit="cup" ingredient="sour cream" status="warning" type="pantry"/>
+        <Ingredient quantity="1" unit="cup" ingredient="Saffron" status="critical" type="pantry"/>
+        <Ingredient quantity="1" unit="cup" ingredient="Salt" type="pantry"/>
       </ul>
     </Layout>
   )
