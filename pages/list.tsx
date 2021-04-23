ecf7d6f8fe34b26420e7b633b06826ce67cded97
wingmatt <matt@wingwebhelp.com>
2021-04-22 22:14:39 -0700

    Specify H2 underline is for Shopping List only. Adjust font weights

--- pages/list.tsx
+++ pages/list.tsx
@@ -1,32 +1,34 @@
 import Layout from '../components/Layout'
 import ShoppingListItem from '../components/ShoppingListItem'
+import styles from "../styles/components/ShoppingListItem.module.css";
+
 
 export default function List(){
   return (
     <Layout title="Shopping List">
       <ul className="content-list">
-        <h2>Produce</h2>
+        <h2 className={styles.category}>Produce</h2>
         <ShoppingListItem quantity="1" unit="cup" ingredient="sugar" type="shopping" />
         <ShoppingListItem quantity="1" unit="cup" ingredient="sour cream" type="shopping"/>
         <ShoppingListItem quantity="1" unit="cup" ingredient="Saffron" type="shopping"/>
         <ShoppingListItem quantity="1" unit="cup" ingredient="Salt" type="shopping"/>
       </ul>
       <ul className="content-list">
-        <h2>Spices</h2>
+        <h2 className={styles.category}>Spices</h2>
         <ShoppingListItem quantity="1" unit="cup" ingredient="sugar" type="shopping" />
         <ShoppingListItem quantity="1" unit="cup" ingredient="sour cream" type="shopping"/>
         <ShoppingListItem quantity="1" unit="cup" ingredient="Saffron" type="shopping"/>
         <ShoppingListItem quantity="1" unit="cup" ingredient="Salt" type="shopping"/>
       </ul>
       <ul className="content-list">
-        <h2>Meat</h2>
+        <h2 className={styles.category}>Meat</h2>
         <ShoppingListItem quantity="1" unit="cup" ingredient="sugar" type="shopping" />
         <ShoppingListItem quantity="1" unit="cup" ingredient="sour cream" type="shopping"/>
         <ShoppingListItem quantity="1" unit="cup" ingredient="Saffron" type="shopping"/>
         <ShoppingListItem quantity="1" unit="cup" ingredient="Salt" type="shopping"/>
       </ul>
       <ul className="content-list">
-        <h2>Dairy</h2>
+        <h2 className={styles.category}>Dairy</h2>
         <ShoppingListItem quantity="1" unit="cup" ingredient="sugar" type="shopping" />
         <ShoppingListItem quantity="1" unit="cup" ingredient="sour cream" type="shopping"/>
         <ShoppingListItem quantity="1" unit="cup" ingredient="Saffron" type="shopping"/>
