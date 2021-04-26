67f7f2177c35ec01f7b3296d1cf4f5c22d07c182
wingmatt <matt@wingwebhelp.com>
2021-04-25 22:28:20 -0700

    InputActions -> PantryActions. Remove switch case

--- components/PantryIngredient.tsx
+++ components/PantryIngredient.tsx
@@ -5,7 +5,7 @@ import styles from '../styles/components/Ingredient.module.css'
 import EditableInput from './EditableInput'
 import {FiEdit, FiRepeat} from 'react-icons/fi'
 
-const InputActions = (props: {
+const PantryActions = (props: {
   type: IngredientInterface["type"],
   className: string,
   editing: any
@@ -14,32 +14,15 @@ const InputActions = (props: {
     // @ts-ignore: Initializer provides no value for this binding element and the binding element has no default value.
     editing: [editing, setEditing]
   } = {
-    editing: useState(false), // This also works if we just set it to props.editing
-    ...(props.editing || {}) // This is apparently crucial to passing the state through
+    editing: useState(false),
+    ...(props.editing || {})
   }
-  switch (props.type) {
-    case 'pantry':
-      return (
-        <div className={styles.actions}>
-          <button onClick={() => setEditing(!editing)}><FiEdit/>Edit</button>
-          <button><FiRepeat/>Reorder</button>
-        </div>
-      )
-    case 'shopping':
-      return (
-        <div className={styles.actions}>
-          <button onClick={() => setEditing(!editing)}>Edit</button>
-          <button>Remove</button>
-        </div>
-      )
-    case 'recipe':
-      return (
-        <div className={styles.actions}>
-          <button onClick={() => setEditing(!editing)}>Edit</button>
-          <button>Add to List</button>
-        </div>
-      )
-  }
+  return (
+    <div className={styles.actions}>
+      <button onClick={() => setEditing(!editing)}><FiEdit/>Edit</button>
+      <button><FiRepeat/>Reorder</button>
+    </div>
+  )
 }
 
 export default function Ingredient (props: IngredientInterface) {
@@ -52,7 +35,7 @@ export default function Ingredient (props: IngredientInterface) {
         <EditableInput editing={editing}>At least {props.quantity} {props.unit}</EditableInput>
         <EditableInput editing={editing}>Expires 01/01/1000</EditableInput>
       </ul>
-      <InputActions type={props.type} className={styles.actions} editing={{editing: [editing, setEditing]}} />
+      <PantryActions type={props.type} className={styles.actions} editing={{editing: [editing, setEditing]}} />
     </CollapsiblePanel>
     
   </li>
