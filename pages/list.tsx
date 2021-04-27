42e44ae8f7216f5e7c9cbf979477cbd290032b1a
wingmatt <matt@wingwebhelp.com>
2021-04-26 22:03:12 -0700

    Remove type from ShoppingListItems

--- pages/list.tsx
+++ pages/list.tsx
@@ -8,31 +8,31 @@ export default function List(){
     <Layout title="Shopping List">
       <ul className="content-list">
         <h2 className={styles.category}>Produce</h2>
-        <ShoppingListItem quantity="1" unit="cup" ingredient="sugar" type="shopping" />
-        <ShoppingListItem quantity="1" unit="cup" ingredient="sour cream" type="shopping"/>
-        <ShoppingListItem quantity="1" unit="cup" ingredient="Saffron" type="shopping"/>
-        <ShoppingListItem quantity="1" unit="cup" ingredient="Salt" type="shopping"/>
+        <ShoppingListItem quantity="1" unit="cup" ingredient="sugar" />
+        <ShoppingListItem quantity="1" unit="cup" ingredient="sour cream"/>
+        <ShoppingListItem quantity="1" unit="cup" ingredient="Saffron"/>
+        <ShoppingListItem quantity="1" unit="cup" ingredient="Salt"/>
       </ul>
       <ul className="content-list">
         <h2 className={styles.category}>Spices</h2>
-        <ShoppingListItem quantity="1" unit="cup" ingredient="sugar" type="shopping" />
-        <ShoppingListItem quantity="1" unit="cup" ingredient="sour cream" type="shopping"/>
-        <ShoppingListItem quantity="1" unit="cup" ingredient="Saffron" type="shopping"/>
-        <ShoppingListItem quantity="1" unit="cup" ingredient="Salt" type="shopping"/>
+        <ShoppingListItem quantity="1" unit="cup" ingredient="sugar" />
+        <ShoppingListItem quantity="1" unit="cup" ingredient="sour cream"/>
+        <ShoppingListItem quantity="1" unit="cup" ingredient="Saffron"/>
+        <ShoppingListItem quantity="1" unit="cup" ingredient="Salt"/>
       </ul>
       <ul className="content-list">
         <h2 className={styles.category}>Meat</h2>
-        <ShoppingListItem quantity="1" unit="cup" ingredient="sugar" type="shopping" />
-        <ShoppingListItem quantity="1" unit="cup" ingredient="sour cream" type="shopping"/>
-        <ShoppingListItem quantity="1" unit="cup" ingredient="Saffron" type="shopping"/>
-        <ShoppingListItem quantity="1" unit="cup" ingredient="Salt" type="shopping"/>
+        <ShoppingListItem quantity="1" unit="cup" ingredient="sugar" />
+        <ShoppingListItem quantity="1" unit="cup" ingredient="sour cream"/>
+        <ShoppingListItem quantity="1" unit="cup" ingredient="Saffron"/>
+        <ShoppingListItem quantity="1" unit="cup" ingredient="Salt"/>
       </ul>
       <ul className="content-list">
         <h2 className={styles.category}>Dairy</h2>
-        <ShoppingListItem quantity="1" unit="cup" ingredient="sugar" type="shopping" />
-        <ShoppingListItem quantity="1" unit="cup" ingredient="sour cream" type="shopping"/>
-        <ShoppingListItem quantity="1" unit="cup" ingredient="Saffron" type="shopping"/>
-        <ShoppingListItem quantity="1" unit="cup" ingredient="Salt" type="shopping"/>
+        <ShoppingListItem quantity="1" unit="cup" ingredient="sugar" />
+        <ShoppingListItem quantity="1" unit="cup" ingredient="sour cream"/>
+        <ShoppingListItem quantity="1" unit="cup" ingredient="Saffron"/>
+        <ShoppingListItem quantity="1" unit="cup" ingredient="Salt"/>
       </ul>
     </Layout>
   )
