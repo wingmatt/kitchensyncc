337fc38d73331bb265d862e596470489d45b3bd0
wingmatt <matt@wingwebhelp.com>
2021-04-26 17:30:08 -0700

    Thin out collapse/expand indicator

--- components/CollapsiblePanel.tsx
+++ components/CollapsiblePanel.tsx
@@ -8,8 +8,8 @@ export default function CollapsiblePanel (props) {
     <div className="collapsible-panel">
       <h2 onClick={() => setExpanded(!expanded)}>
         <button aria-expanded={expanded}><IngredientStatus status={props.status}/><span>{props.title}</span><svg viewBox="0 0 10 10" focusable="false">
-          <rect className="vert" height="8" width="2" y="1" x="4"/>
-          <rect height="2" width="8" y="4" x="1" />
+          <rect className="vert" height="8" width="1" y="1" x="4.5"/>
+          <rect height="1" width="8" y="4.5" x="1" />
         </svg></button>
         
       </h2>
