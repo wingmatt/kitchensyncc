fb9615a36032b7dec50626afef7dafb9dae37ff4
wingmatt <matt@wingwebhelp.com>
2021-04-26 22:00:17 -0700

    Add tinkering notes back

--- components/PantryIngredient.tsx
+++ components/PantryIngredient.tsx
@@ -13,8 +13,8 @@ const PantryActions = (props: {
     // @ts-ignore: Initializer provides no value for this binding element and the binding element has no default value.
     editing: [editing, setEditing]
   } = {
-    editing: useState(false),
-    ...(props.editing || {})
+    editing: useState(false), // This also works if we just set it to props.editing
+    ...(props.editing || {}) // This is apparently crucial to passing the state through
   }
   return (
     <div className={styles.actions}>
