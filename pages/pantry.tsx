6a017e73e318a1e071ed1e6f0ad2f043cbe69b22
wingmatt <matt@wingwebhelp.com>
2021-04-26 17:27:26 -0700

    Replace Ingredient w/ PantryIngredient

--- pages/pantry.tsx
+++ pages/pantry.tsx
@@ -1,14 +1,14 @@
 import Layout from '../components/Layout'
-import Ingredient from '../components/Ingredient'
+import PantryIngredient from '../components/PantryIngredient'
 
 export default function Pantry(){
   return (
     <Layout title="Pantry">
       <ul className="content-list">
-        <Ingredient quantity="1" unit="cup" ingredient="sugar" status="ok" type="pantry"/>
-        <Ingredient quantity="1" unit="cup" ingredient="sour cream" status="warning" type="pantry"/>
-        <Ingredient quantity="1" unit="cup" ingredient="Saffron" status="critical" type="pantry"/>
-        <Ingredient quantity="1" unit="cup" ingredient="Salt" type="pantry"/>
+        <PantryIngredient quantity="1" unit="cup" ingredient="sugar" status="ok"/>
+        <PantryIngredient quantity="1" unit="cup" ingredient="sour cream" status="warning"/>
+        <PantryIngredient quantity="1" unit="cup" ingredient="Saffron" status="critical"/>
+        <PantryIngredient quantity="1" unit="cup" ingredient="Salt"/>
       </ul>
     </Layout>
   )
