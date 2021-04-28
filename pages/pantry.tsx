aad497d3dc285288cd6510139c7ed3da0014b1db
wingmatt <matt@wingwebhelp.com>
2021-04-27 21:29:37 -0700

    PantryIngredient component -> PantryItem

--- pages/pantry.tsx
+++ pages/pantry.tsx
@@ -1,14 +1,14 @@
 import Layout from '../components/Layout'
-import PantryIngredient from '../components/PantryIngredient'
+import PantryItem from '../components/PantryItem'
 
 export default function Pantry(){
   return (
     <Layout title="Pantry">
       <ul className="content-list">
-        <PantryIngredient quantity="1" unit="cup" ingredient="sugar" status="ok"/>
-        <PantryIngredient quantity="1" unit="cup" ingredient="sour cream" status="warning"/>
-        <PantryIngredient quantity="1" unit="cup" ingredient="Saffron" status="critical"/>
-        <PantryIngredient quantity="1" unit="cup" ingredient="Salt"/>
+        <PantryItem quantity="1" unit="cup" ingredient="sugar" status="ok"/>
+        <PantryItem quantity="1" unit="cup" ingredient="sour cream" status="warning"/>
+        <PantryItem quantity="1" unit="cup" ingredient="Saffron" status="critical"/>
+        <PantryItem quantity="1" unit="cup" ingredient="Salt"/>
       </ul>
     </Layout>
   )
