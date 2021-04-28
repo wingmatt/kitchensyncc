2ffa973924f55e3a0e7c8e07036c0ef3cdf2e933
wingmatt <matt@wingwebhelp.com>
2021-04-27 21:52:43 -0700

    Add external link indicator

--- components/Recipe.tsx
+++ components/Recipe.tsx
@@ -2,13 +2,14 @@ import { Recipe as RecipeInterface } from "../types";
 import styles from "../styles/components/Recipe.module.css"
 import IngredientList from "./IngredientList";
 import CollapsiblePanel from "./CollapsiblePanel"
+import { FiExternalLink } from "react-icons/fi"
 
 export default function Recipe(props: RecipeInterface) {
   
   return (
     <article className={styles.recipe}>
       <CollapsiblePanel title={props.name}>
-        <a href={props.url} className={"button " + styles.button}>Go to Recipe</a>
+        <a href={props.url} className={"button " + styles.button}><FiExternalLink/>Go to Recipe </a>
         <IngredientList title="Ingredients" ingredients={props.ingredients} />
       </CollapsiblePanel>
     </article>
