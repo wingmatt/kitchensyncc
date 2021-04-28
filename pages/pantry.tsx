48760e5d9a10cf21970549c1164f3362dbacb690
wingmatt <matt@wingwebhelp.com>
2021-04-27 21:36:19 -0700

    Create ItemGroup component. Use on Pantry page

--- pages/pantry.tsx
+++ pages/pantry.tsx
@@ -1,15 +1,16 @@
 import Layout from '../components/Layout'
+import ItemGroup from '../components/ItemGroup'
 import PantryItem from '../components/PantryItem'
 
 export default function Pantry(){
   return (
     <Layout title="Pantry">
-      <ul className="content-list">
+      <ItemGroup title="Produce">
         <PantryItem quantity="1" unit="cup" ingredient="sugar" status="ok"/>
         <PantryItem quantity="1" unit="cup" ingredient="sour cream" status="warning"/>
         <PantryItem quantity="1" unit="cup" ingredient="Saffron" status="critical"/>
         <PantryItem quantity="1" unit="cup" ingredient="Salt"/>
-      </ul>
+      </ItemGroup>
     </Layout>
   )
 }
