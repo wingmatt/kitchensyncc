588c77edfd5ee49ce945cdd192b810d4f813ad8b
wingmatt <matt@wingwebhelp.com>
2021-04-29 08:07:19 -0700

    Render FloatingButton without styling. Add labelFromAction function to generate label & icon based on action

--- components/FloatingButton.tsx
+++ components/FloatingButton.tsx
@@ -1,15 +1,29 @@
 import {FloatingButton as FloatingButtonInterface} from '../types'
+import {FiEdit, FiCheck} from 'react-icons/fi'
 
-const labelFromAction = (action: FloatingButtonInterface["action"]) => {
+const labelFromAction = (action: FloatingButtonInterface["action"]): FloatingButtonInterface => {
   switch (action) {
-    case '':
+    case 'editShoppingList':
+      return {
+        action: action,
+        label: "Edit",
+        icon: <FiEdit/>
+      }
+    case 'updateShoppingList':
+      return {
+        action: action,
+        label: "Update",
+        icon: <FiCheck/>
+      }
   }
 }
 
 export default function FloatingButton (props) {
-  const {label, color, icon} = labelFromAction(props.action)
+  const {action, label, icon} = labelFromAction(props.action)
   
   return (
-
+    <button className={action}>
+      {icon}{label}
+    </button>
   )
 }
