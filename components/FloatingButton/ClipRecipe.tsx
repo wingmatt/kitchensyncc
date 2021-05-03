d69f2b102c95c64fee1595f545eba81699148ffd
wingmatt <matt@wingwebhelp.com>
2021-05-02 22:09:45 -0700

    RecipeSearch tweaks. Add scroll to form on button

--- components/FloatingButton/ClipRecipe.tsx
+++ components/FloatingButton/ClipRecipe.tsx
@@ -2,11 +2,11 @@ import FloatingButton from './'
 import { FiScissors } from 'react-icons/fi'
 
 const clipRecipe = () => {
-
+  location.hash = "#recipe-search"
 }
 
 export default function ClipRecipe () {
   return (
-    <FloatingButton action="clipRecipe" label="Clip Recipe" icon={<FiScissors focusable="false"/>} />
+    <FloatingButton action="clipRecipe" label="Clip Recipe" icon={<FiScissors focusable="false"/>} onClick={clipRecipe()}/>
   )
 }
