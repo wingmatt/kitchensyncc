2345c90667a931cc8d9dc616b46628fd908f69a4
wingmatt <matt@wingwebhelp.com>
2021-05-02 17:30:55 -0700

    Create floating buttons for known context actions

---
+++ components/FloatingButton/SearchRecipes.tsx
@@ -0,0 +1,12 @@
+import FloatingButton from './'
+import { FiSearch } from 'react-icons/fi'
+
+const searchRecipes = () => {
+  
+}
+
+export default function SearchRecipes () {
+  return (
+    <FloatingButton action="searchRecipes" label="Search Recipes" icon={<FiSearch/>} />
+  )
+}
