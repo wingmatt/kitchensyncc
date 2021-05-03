8feb6a553bee5d16aca7ff87c4c00a36edb69160
wingmatt <matt@wingwebhelp.com>
2021-05-02 17:04:03 -0700

    Try new file structure for FloatingButtons

---
+++ components/FloatingButton/index.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/components/FloatingButton.module.css'
+
+export default function FloatingButton (props) {
+   return (
+    <button className={styles[props.action]} title={props.label} >
+      {props.icon}{props.label}
+    </button>
+  )
+}
