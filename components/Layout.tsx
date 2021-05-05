c6802e3b90f46c9d21212ad1c1b73a03c9a74792
wingmatt <matt@wingwebhelp.com>
2021-05-04 21:56:41 -0700

    Add skip nav link

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,6 +2,9 @@ import Head from "next/head";
 import Menu from "./Menu"
 import styles from "../styles/Home.module.css";
 
+import { SkipNavLink, SkipNavContent } from "@reach/skip-nav";
+import "@reach/skip-nav/styles.css";
+
 export default function Layout({ children, title = "Kitchen Syncc" }) {
   return (
     <div className={styles.container}>
@@ -9,11 +12,13 @@ export default function Layout({ children, title = "Kitchen Syncc" }) {
         <title>{title}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <SkipNavLink/>
       <h1 className={styles.title}>
           <a href="/">{title}</a>
       </h1>
       <Menu/>
       <main className={styles.main}>
+        <SkipNavContent/>
         {children}
       </main>
     </div>
