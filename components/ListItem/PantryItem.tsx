62f8f005bd36b46205484a14fb700b79b2806036
wingmatt <matt@wingwebhelp.com>
2021-05-06 17:17:56 -0700

    Use IngredientUnitInput for Unit input. Run Prettier formatting

--- components/ListItem/PantryItem.tsx
+++ components/ListItem/PantryItem.tsx
@@ -1,54 +1,76 @@
-import { useState } from 'react'
-import {Ingredient as IngredientInterface} from '../../types'
-import CollapsiblePanel from '../CollapsiblePanel'
-import styles from '../../styles/components/Ingredient.module.css'
-import EditableInput from '../EditableInput'
-import {FiEdit, FiRepeat} from 'react-icons/fi'
+import { useState } from "react";
+import { Ingredient as IngredientInterface } from "../../types";
+import CollapsiblePanel from "../CollapsiblePanel";
+import styles from "../../styles/components/Ingredient.module.css";
+import EditableInput from "../EditableInput";
+import IngredientUnitInput from "../EditableInput/IngredientUnitInput"
+import { FiEdit, FiRepeat } from "react-icons/fi";
 
-const PantryActions = (props: {
-  className: string,
-  editing: any
-}) => {
+const PantryActions = (props: { className: string; editing: any }) => {
   const {
     // @ts-ignore: Initializer provides no value for this binding element and the binding element has no default value.
-    editing: [editing, setEditing]
+    editing: [editing, setEditing],
   } = {
     editing: useState(false), // This also works if we just set it to props.editing
-    ...(props.editing || {}) // This is apparently crucial to passing the state through
-  }
+    ...(props.editing || {}), // This is apparently crucial to passing the state through
+  };
   return (
     <div className={styles.actions}>
-      <button onClick={() => setEditing(!editing)}><FiEdit focusable="false"/>Edit</button>
-      <button><FiRepeat focusable="false" />Reorder</button>
+      <button onClick={() => setEditing(!editing)}>
+        <FiEdit focusable="false" />
+        Edit
+      </button>
+      <button>
+        <FiRepeat focusable="false" />
+        Reorder
+      </button>
     </div>
-  )
-}
+  );
+};
 
 const statusToLabel = (status) => {
   switch (status) {
     case "ok":
-      return "Have"
+      return "Have";
     case "warning":
-      return "Need to Use"
+      return "Need to Use";
     case "critical":
-      return "Expired"
+      return "Expired";
   }
-}
+};
 
-export default function PantryItem (props: IngredientInterface) {
-  const [editing, setEditing] = useState(false)
-  
+export default function PantryItem(props: IngredientInterface) {
+  const [editing, setEditing] = useState(false);
+
   return (
-  <li className={styles.ingredient}>
-    <CollapsiblePanel title={ props.ingredient} status={props.status} statusLabel={statusToLabel(props.status)}>
-      <ul>
-        <EditableInput label="Amount" type="number" editing={editing}>{props.quantity}</EditableInput>
-        <EditableInput label="Unit" type="text" editing={editing}>{props.unit}</EditableInput>
-        <EditableInput label="Expires" type="date"editing={editing}>01/01/1000</EditableInput>
-      </ul>
-      <PantryActions className={styles.actions} editing={{editing: [editing, setEditing]}} />
-    </CollapsiblePanel>
-    
-  </li>
-  )
+    <li className={styles.ingredient}>
+      <CollapsiblePanel
+        title={props.ingredient}
+        status={props.status}
+        statusLabel={statusToLabel(props.status)}
+      >
+        <ul>
+          <li>
+            Amount: 
+            <EditableInput label="Amount" type="number" editing={editing}>
+              {props.quantity}
+            </EditableInput>
+            <IngredientUnitInput label="Unit" type="text" editing={editing}>
+              {props.unit}
+            </IngredientUnitInput>
+          </li>
+          <li>
+            Expires: 
+            <EditableInput label="Expires" type="date" editing={editing}>
+              01/01/1000
+            </EditableInput>
+          </li>
+        </ul>
+        <PantryActions
+          className={styles.actions}
+          editing={{ editing: [editing, setEditing] }}
+        />
+      </CollapsiblePanel>
+    </li>
+  );
 }
