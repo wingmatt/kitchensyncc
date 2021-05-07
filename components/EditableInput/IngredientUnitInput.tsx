c86b6aee3759e34e6022394ca235d35b319ad790
wingmatt <matt@wingwebhelp.com>
2021-05-06 17:17:11 -0700

    Add @reach/ComboBox component

---
+++ components/EditableInput/IngredientUnitInput.tsx
@@ -0,0 +1,28 @@
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
+import "@reach/combobox/styles.css";
+
+export default function IngredientUnitInput(props) {
+  return (
+    <label>
+      <span className="sr-only">{props.label}</span>
+      <Combobox>
+        <ComboboxInput />
+        <ComboboxPopover>
+          <ComboboxList>
+            <ComboboxOption value="Cup" />
+            <ComboboxOption value="Clove" />
+            <ComboboxOption value="Ounce" />
+            <ComboboxOption value="Teaspoon" /> 
+            <ComboboxOption value="Tablespoon" />
+          </ComboboxList>
+        </ComboboxPopover>
+      </Combobox>
+    </label>
+  );
+}
