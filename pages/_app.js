1ec25604d45d79dc0c8a7c34e07be92b424497a2
wingmatt <matt@wingwebhelp.com>
2021-05-10 21:27:19 -0700

    Add Amplify UI pkg. Wrap app in withAuthenticator

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
 import '../styles/globals.css'
+import {withAuthenticator} from '@aws-amplify/ui-react'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default withAuthenticator(MyApp)
