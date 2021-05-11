647d194cf73bd1ce52c572db7b99ebf096ac99ab
wingmatt <matt@wingwebhelp.com>
2021-05-10 22:02:17 -0700

    Add AWS Config to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import '../styles/globals.css'
 import {withAuthenticator} from '@aws-amplify/ui-react'
+import Amplify, { Auth } from 'aws-amplify';
+import awsconfig from '../src/aws-exports';
+Amplify.configure(awsconfig);
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
