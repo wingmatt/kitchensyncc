7f680d6565ba1558e09938874ff7e6e4053c5615
wingmatt <matt@wingwebhelp.com>
2021-07-15 08:02:32 -0700

    Add case for rendering PantryItem #17 Convert if statement to a switch case statement

--- components/ListItem/Item.tsx
+++ components/ListItem/Item.tsx
@@ -1,8 +1,12 @@
 import ShoppingListItem from "./ShoppingListItem";
+import PantryItem from "./PantryItem"
 
 const Item = (props) => {
-  if (props.type == 'shoppingDetails') {
-    return <ShoppingListItem quantity={props.quantity} unit={props.unit} ingredient={props.ingredient} status={props.status}/>
+  switch (props.type) {
+    case 'shoppingDetails':
+      return <ShoppingListItem quantity={props.quantity} unit={props.unit} ingredient={props.ingredient} status={props.status}/>
+    case 'pantryDetails':
+      return <PantryItem quantity={props.quantity} unit={props.unit} ingredient={props.ingredient} status={props.status}/>
   }
 }
 
